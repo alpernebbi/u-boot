2aae093c9a830645f840af40012edbc93699459a
Elaine Zhang <zhangqing@rock-chips.com>
2019-01-22 16:04:28 +0800

    clk: rockchip: rk3399: print arm enter and init rate
    Change-Id: Ib5e3e0f9a3e1a5b535ec852e7c58966dc0db77cf
    Signed-off-by: Elaine Zhang <zhangqing@rock-chips.com>

--- arch/arm/include/asm/arch-rockchip/cru_rk3399.h
+++ arch/arm/include/asm/arch-rockchip/cru_rk3399.h
@@ -9,6 +9,14 @@
 /* Private data for the clock driver - used by rockchip_get_cru() */
 struct rk3399_clk_priv {
 	struct rockchip_cru *cru;
+	ulong armlclk_hz;
+	ulong armlclk_enter_hz;
+	ulong armlclk_init_hz;
+	ulong armbclk_hz;
+	ulong armbclk_enter_hz;
+	ulong armbclk_init_hz;
+	bool sync_kernel;
+	bool set_armclk_rate;
 };
 
 struct rk3399_pmuclk_priv {
