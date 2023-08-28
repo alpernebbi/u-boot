92a1c233023c19709ae5794922e81440e23e1905
Alper Nebi Yasak <alpernebiyasak@gmail.com>
2023-08-28 16:59:07 +0300

    fixup! riscv: qemu: Enable ramfb by default

--- board/emulation/qemu-riscv/qemu-riscv.c
+++ board/emulation/qemu-riscv/qemu-riscv.c
@@ -29,46 +29,6 @@ int is_flash_available(void)
 }
 #endif
 
-int board_early_init_f(void)
-{
-	struct udevice *dev;
-	int ret;
-
-	/*
-	 * Make sure we enumerate the QEMU Firmware device to bind ramfb
-	 * so video_reserve() can reserve memory for it.
-	 */
-	if (IS_ENABLED(CONFIG_QFW)) {
-		ret = qfw_get_dev(&dev);
-		if (ret) {
-			log_err("Failed to get QEMU FW device: %d\n", ret);
-			return ret;
-		}
-	}
-
-	return 0;
-}
-
-int board_early_init_r(void)
-{
-	struct udevice *dev;
-	int ret;
-
-	/*
-	 * Make sure we enumerate the QEMU Firmware device to find ramfb
-	 * before console_init.
-	 */
-	if (IS_ENABLED(CONFIG_QFW)) {
-		ret = qfw_get_dev(&dev);
-		if (ret) {
-			log_err("Failed to get QEMU FW device: %d\n", ret);
-			return ret;
-		}
-	}
-
-	return 0;
-}
-
 int board_init(void)
 {
 	return 0;
