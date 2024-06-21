58fbbc2e574f40f54049783492d45c901a620682
Alper Nebi Yasak <alpernebiyasak@gmail.com>
2024-06-21 14:57:15 +0300

    WIP: Add custom board_get_usable_ram_top
    Shifts down the relocation address so we don't overwrite the coreboot
    table.

--- board/coreboot/coreboot-arm/coreboot-arm.c
+++ board/coreboot/coreboot-arm/coreboot-arm.c
@@ -88,6 +88,48 @@ int board_late_init(void)
 	return 0;
 }
 
+/*
+ * This function looks for the highest region of memory lower than 4GB which
+ * has enough space for U-Boot where U-Boot is aligned on a page boundary. It
+ * overrides the default implementation found elsewhere which simply picks the
+ * end of ram, wherever that may be. The location of the stack, the relocation
+ * address, and how far U-Boot is moved by relocation are set in the global
+ * data structure.
+ */
+phys_addr_t board_get_usable_ram_top(phys_size_t total_size)
+{
+	uintptr_t dest_addr = 0;
+	int i;
+
+	for (i = 0; i < lib_sysinfo.n_memranges; i++) {
+		struct memrange *memrange = &lib_sysinfo.memrange[i];
+		/* Force U-Boot to relocate to a page aligned address. */
+		uint64_t start = roundup(memrange->base, 1 << 12);
+		uint64_t end = memrange->base + memrange->size;
+
+		/* Ignore non-memory regions. */
+		if (memrange->type != CB_MEM_RAM)
+			continue;
+
+		/* Filter memory over 4GB. */
+		if (end > 0xffffffffULL)
+			end = 0x100000000ULL;
+		/* Skip this region if it's too small. */
+		if (end - start < total_size)
+			continue;
+
+		/* Use this address if it's the largest so far. */
+		if (end > dest_addr)
+			dest_addr = end;
+	}
+
+	/* If no suitable area was found, return an error. */
+	if (!dest_addr)
+		panic("No available memory found for relocation");
+
+	return (ulong)dest_addr;
+}
+
 int dram_init(void)
 {
 	if (fdtdec_setup_mem_size_base() != 0)
